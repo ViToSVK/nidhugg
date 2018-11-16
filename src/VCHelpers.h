84c603acdc66c7b49098441554dabf6f220df0c2
Viktor Toman <vtoman07@gmail.com>
2018-11-16 21:47:42 +0100

    helpers for nodes

--- src/VCHelpers.h
+++ src/VCHelpers.h
@@ -5,63 +5,96 @@
 #include <llvm/IR/Function.h>
 
 #include "VCEvent.h"
+#include "VCBasis.h"
 
 /* helper functions */
 inline bool isLoad(const VCEvent *ev) {
+  assert(ev);
   return ev->kind == VCEvent::Kind::LOAD;
 }
 inline bool isLoad(const VCEvent& ev) {
   return ev.kind == VCEvent::Kind::LOAD;
 }
+inline bool isLoad(const Node *nd) {
+  assert(nd);
+  return isLoad(nd->getEvent());
+}
 
 //
 
 inline bool isStore(const VCEvent *ev) {
+  assert(ev);
   return ev->kind == VCEvent::Kind::STORE;
 }
 inline bool isStore(const VCEvent& ev) {
   return ev.kind == VCEvent::Kind::STORE;
 }
+inline bool isStore(const Node *nd) {
+  assert(nd);
+  return isStore(nd->getEvent());
+}
+
 
 //
 
 inline bool isSpawn(const VCEvent *ev) {
+  assert(ev);
   return ev->kind == VCEvent::Kind::SPAWN;
 }
 inline bool isSpawn(const VCEvent& ev) {
   return ev.kind == VCEvent::Kind::SPAWN;
 }
+inline bool isSpawn(const Node *nd) {
+  assert(nd);
+  return isSpawn(nd->getEvent());
+}
 
 //
 
 inline bool isJoin(const VCEvent *ev) {
+  assert(ev);
   return ev->kind == VCEvent::Kind::JOIN;
 }
 inline bool isJoin(const VCEvent& ev) {
   return ev.kind == VCEvent::Kind::JOIN;
 }
+inline bool isJoin(const Node *nd) {
+  assert(nd);
+  return isJoin(nd->getEvent());
+}
 
 //
 
 inline bool isMutexInit(const VCEvent *ev) {
+  assert(ev);
   return ev->kind == VCEvent::Kind::M_INIT;
 }
 inline bool isMutexInit(const VCEvent& ev) {
   return ev.kind == VCEvent::Kind::M_INIT;
 }
+inline bool isMutexInit(const Node *nd) {
+  assert(nd);
+  return isMutexInit(nd->getEvent());
+}
 
 //
 
 inline bool isMutexDestroy(const VCEvent *ev) {
+  assert(ev);
   return ev->kind == VCEvent::Kind::M_DESTROY;
 }
 inline bool isMutexDestroy(const VCEvent& ev) {
   return ev.kind == VCEvent::Kind::M_DESTROY;
 }
+inline bool isMutexDestroy(const Node *nd) {
+  assert(nd);
+  return isMutexDestroy(nd->getEvent());
+}
 
 //
 
 inline bool isLock(const VCEvent *ev) {
+  assert(ev);
   return (ev->kind == VCEvent::Kind::M_LOCK ||
           ev->kind == VCEvent::Kind::M_LOCKATTEMPT);
 }
@@ -69,34 +102,51 @@ inline bool isLock(const VCEvent& ev) {
   return (ev.kind == VCEvent::Kind::M_LOCK ||
           ev.kind == VCEvent::Kind::M_LOCKATTEMPT);
 }
+inline bool isLock(const Node *nd) {
+  assert(nd);
+  return isLock(nd->getEvent());
+}
 
 //
 
 inline bool isUnlock(const VCEvent *ev) {
+  assert(ev);
   return ev->kind == VCEvent::Kind::M_UNLOCK;
 }
 inline bool isUnlock(const VCEvent& ev) {
   return ev.kind == VCEvent::Kind::M_UNLOCK;
 }
+inline bool isUnlock(const Node *nd) {
+  assert(nd);
+  return isUnlock(nd->getEvent());
+}
 
 //
 
 inline bool isRead(const VCEvent *ev) {
+  assert(ev);
   return isLoad(ev); // || isLock(ev) - treat them separately
 }
 inline bool isRead(const VCEvent& ev) {
   return isLoad(ev); // || isLock(ev) - treat them separately
 }
+inline bool isRead(const Node *nd) {
+  assert(nd);
+  return isRead(nd->getEvent());
+}
 
 //
 
-inline bool isWrite(const VCEvent *ev)
-{
+inline bool isWrite(const VCEvent *ev) {
+  assert(ev);
   return isStore(ev); //  || isUnlock(ev) - treat them separately
 }
-inline bool isWrite(const VCEvent& ev)
-{
+inline bool isWrite(const VCEvent& ev) {
   return isStore(ev); //  || isUnlock(ev) - treat them separately
+}
+inline bool isWrite(const Node *nd) {
+  assert(nd);
+  return isWrite(nd->getEvent());
 }
 
 /* llvm instruction helpers */
@@ -116,28 +166,23 @@ inline bool is_function_call(const llvm::Instruction *I, const char *name)
   return true;
 }
 
-inline bool isLock(const llvm::Instruction& I)
-{
+inline bool isLock(const llvm::Instruction& I) {
   return is_function_call(&I, "pthread_mutex_lock");
 }
 
-inline bool isUnlock(const llvm::Instruction& I)
-{
+inline bool isUnlock(const llvm::Instruction& I) {
   return is_function_call(&I, "pthread_mutex_unlock");
 }
 
-inline bool isRead(const llvm::Instruction& I)
-{
+inline bool isRead(const llvm::Instruction& I) {
   return llvm::isa<llvm::LoadInst>(&I) || isUnlock(I);
 }
 
-inline bool isWrite(const llvm::Instruction& I)
-{
+inline bool isWrite(const llvm::Instruction& I) {
   return llvm::isa<llvm::StoreInst>(&I) || isLock(I);
 }
 
-inline bool isGlobalLoad(const llvm::Instruction *I)
-{
+inline bool isGlobalLoad(const llvm::Instruction *I) {
   // XXX: what about pointers!
   return llvm::isa<llvm::LoadInst>(I) &&
           !llvm::isa<llvm::AllocaInst>(I->getOperand(0)->stripInBoundsOffsets());
