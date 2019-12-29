f4b53cb3843e5f7e43c03fb20987e0777b8a6e4d
Viktor Toman <vtoman07@gmail.com>
2019-12-29 14:37:19 +0100

    Closure: untabify

--- src/ZClosure.cpp
+++ src/ZClosure.cpp
@@ -23,13 +23,14 @@
 // Got ZHelper due to header -> Graph -> AnnotationNeg -> Annotation -> Helper
 
 // return writeBuffer, writeMemory for obs
-std::pair<const ZEvent *, const ZEvent *> ZClosure::getObs(const ZObs& obs){
-	if(obs.thr == INT_MAX) return {nullptr,nullptr};
-	auto buffer_part = ba.getEvent(obs.thr, -1, obs.ev);
-	assert(isWriteB(buffer_part));
-	auto write_part = buffer_part->write_other_ptr;
-	assert(isWriteM(write_part));
-	return {buffer_part,write_part};
+std::pair<const ZEvent *, const ZEvent *> ZClosure::getObs(const ZObs& obs) {
+  if(obs.thr == INT_MAX)
+    return {nullptr,nullptr};
+  auto buffer_part = ba.getEvent(obs.thr, -1, obs.ev);
+  assert(isWriteB(buffer_part));
+  auto write_part = buffer_part->write_other_ptr;
+  assert(isWriteM(write_part));
+  return {buffer_part,write_part};
 }
 
 
@@ -39,9 +40,9 @@ std::pair<const ZEvent *, const ZEvent *> ZClosure::getObs(const ZObs& obs){
 
 // first) true iff impossible
 // second) true iff something changed
-std::pair<bool, bool> ZClosure::ruleOne(const ZEvent *read, const ZObs& obs){
-	// Seems like done in preclose
-	// Recheck of impossible or change
+std::pair<bool, bool> ZClosure::ruleOne(const ZEvent *read, const ZObs& obs) {
+  // Seems like done in preclose
+  // Recheck of impossible or change
   return {false, false}; // done, no change
 }
 
@@ -52,66 +53,66 @@ std::pair<bool, bool> ZClosure::ruleOne(const ZEvent *read, const ZObs& obs){
 
 // first) true iff impossible
 // second) true iff something changed
-//# warning Initial event is not special case right?
-std::pair<bool, bool> ZClosure::ruleTwo(const ZEvent *read, const ZObs& obs){
-	// Optimization idea if obs different thread then only once this function could be called
-	// as no memory write will be added in between other rules
-	// To do so we can call it preclose and then in rules don't call again
+std::pair<bool, bool> ZClosure::ruleTwo(const ZEvent *read, const ZObs& obs) {
+  // Optimization idea: if obs different thread then only once this function could be called
+  // as no memory write will be added in between other rules
+  // To do so we can call it preclose and then in rules don't call again
 
-	// Without optimization
-	auto write = getObs(obs);
-	auto write_memory = write.second;
-	assert(!write_memory || isWriteM(write_memory));
-	// Idea: Iterate on all (but self) threads and get memory pred of r
-	// add cond that writeM occur after aboce memory write
-	unsigned totThreads =  ba.number_of_threads();
-	unsigned readThread = read->threadID();
-	unsigned writeThread = write_memory ? write_memory->threadID():INT_MAX;
-	auto location = read->ml; // SymAddrSize of read
-	bool change = false;
-	for(unsigned i=0; i < totThreads; ++i){  // looping over all threads
-		if(i != readThread and i != writeThread){	// all but read thread
-			int readaux = ba.auxForMl(location, i);
-			if(readaux == -1) continue;
-			int lastEvid = po.pred(read,i,readaux).second ; // Hardcode Aux gets Event id of pred
-			if(lastEvid != -1){ // {0,1,2,..,lastEvid} happens before real
-				auto memory_pred = gr.getTailW(location,i,lastEvid);	// last write of thread i on same location as read
-				if(memory_pred){	// not nullptr
-					assert(isWriteM(memory_pred));
-					if(!write_memory || po.hasEdge(write_memory,memory_pred)) return {true,false}; // Already reverse edge
-        			if(!po.hasEdge(memory_pred,write_memory)){
-						po.addEdge(memory_pred,write_memory);
-						change = true;
-						added_edges++;
-					}
-				}
-			}
-		}
-	}
-	if(write_memory and readThread != writeThread){ // checking for impossibility due to write thread
-		int writeaux = ba.auxForMl(location, writeThread);
-		if(writeaux != -1){
-			int lastEvid = po.pred(read,writeThread,writeaux).second ; // Hardcode Aux gets Event id of pred
-			if(lastEvid != -1){ // {0,1,2,..,lastEvid} happens before real
-				auto memory_pred = gr.getTailW(location,writeThread,lastEvid);	// last write of thread i on same location as read
-				if(memory_pred){	// not nullptr
-					assert(isWriteM(memory_pred));
-					if(memory_pred->eventID() > write_memory->eventID()) return {true,false}; // Already reverse edge
-				}
-			}
-		}
-	}
-	//std::cout<<"Rule2 "<<change<<"\n";
-	//po.dump();
-	return {false, change}; // done, no change
+  auto write = getObs(obs);
+  auto write_memory = write.second;
+  assert(!write_memory || isWriteM(write_memory));
+  // Idea: Iterate on all (but self) threads, in each thread i:
+  // get 'memory_pred' - conflicting memory-write predecessor of read in thread i
+  // add edge if not already present, that 'memory_pred' -> 'write_memory'
+  unsigned totThreads =  ba.number_of_threads();
+  unsigned readThread = read->threadID();
+  unsigned writeThread = write_memory ? write_memory->threadID() : INT_MAX;
+  auto location = read->ml; // SymAddrSize of read
+  bool change = false;
+  for(unsigned i=0; i < totThreads; ++i) { // looping over all threads
+    if(i != readThread and i != writeThread) { // all but read thread and observation-write thread
+      int readaux = ba.auxForMl(location, i);
+      if(readaux == -1) continue;
+      int lastEvid = po.pred(read,i,readaux).second ; // Hardcode Aux gets Event id of pred
+      if(lastEvid != -1) { // {0,1,2,..,lastEvid} happen before read
+        auto memory_pred = gr.getTailW(location,i,lastEvid); // last write of thread i on same location as read
+        if(memory_pred) { // not nullptr
+          assert(isWriteM(memory_pred));
+          if(!write_memory || po.hasEdge(write_memory,memory_pred))
+            return {true,false}; // Impossible - reverse edge already present
+          if(!po.hasEdge(memory_pred,write_memory)) {
+            po.addEdge(memory_pred,write_memory);
+            change = true;
+            added_edges++;
+          }
+        }
+      }
+    }
+  }
+  if(write_memory and readThread != writeThread) { // checking for impossibility due to write thread
+    int writeaux = ba.auxForMl(location, writeThread);
+    if(writeaux != -1) {
+      int lastEvid = po.pred(read,writeThread,writeaux).second ; // Hardcode Aux gets Event id of pred
+      if(lastEvid != -1) { // {0,1,2,..,lastEvid} happen before read
+        auto memory_pred = gr.getTailW(location,writeThread,lastEvid);  // last write of thread i on same location as read
+        if(memory_pred) { // not nullptr
+          assert(isWriteM(memory_pred));
+          if(memory_pred->eventID() > write_memory->eventID())
+            return {true,false}; // Impossible - reverse edge already present
+        }
+      }
+    }
+  }
+  //po.dump();
+  return {false, change}; // done, change-variable
 }
 
-	// What type of edges are added ?
-		// Rule 1 : obs memory on read (diff thread) , self memory to read
-		// Rule 2 : memory to obs memory (non read thread) if memory < read
-		// Rule 3 : read to memory (non read thread) if obs memory < memory
-	// Rule 3 has to involved again for any type
-	// Seems like Rule 2 happens only once
+  // What type of edges are added ?
+    // Rule 1 : obs memory on read (diff thread) , self memory to read
+    // Rule 2 : memory to obs memory (non read thread) if memory < read
+    // Rule 3 : read to memory (non read thread) if obs memory < memory
+  // Rule 3 has to be involved again for any type
+  // Seems like Rule 2 happens only once
 
 
 /* *************************** */
@@ -120,92 +121,93 @@ std::pair<bool, bool> ZClosure::ruleTwo(const ZEvent *read, const ZObs& obs){
 
 // first) true iff impossible
 // second) true iff something changed
-std::pair<bool, bool> ZClosure::ruleThree(const ZEvent *read, const ZObs& obs){
-	// Without optimization
-	auto write = getObs(obs);
-	auto write_memory = write.second;
-	assert(!write_memory || isWriteM(write_memory));
-	// Idea: Iterate on all (but self and writeM) threads and get memory suc of writeM
-	// add cond that read occur before above memory write
-	// Do same stuff for first suc of writeM using different way
-	unsigned totThreads =  ba.number_of_threads();
-	unsigned readThread = read->threadID();
-	unsigned writeThread = write_memory ? write_memory->threadID():INT_MAX;
-	bool change = false;
-	for(unsigned i=0; i < totThreads; ++i){  // looping over all threads
-		if(i != readThread and i != writeThread){	// all but read thread
-			int lastBefore = gr.getLatestNotAfterIndex(read,i,po);	// {0,1,..,lastBefore} in cache at same ml with r </ wM
-			if(lastBefore == -1) continue;
-			auto &cache = gr.getCache();
-			assert(lastBefore < (int) cache.wm.at(read->ml).at(i).size());
-			if(write_memory){
-				int l=0,r=lastBefore;
-				// Binary search in cache events to get first wM after write
-				while(l<r){ // gives r if fail
-					int mid=(l+r)>>1;
-					auto res = cache.wm.at(read->ml).at(i)[mid];
-					assert(isWriteM(res) && sameMl(res, read) &&
-					    res->threadID() == i && res->auxID() != -1);
-					if(po.hasEdge(write_memory,res)) r=mid;
-					else l=mid+1;
-				} // after the loop, l = r = x
-				auto res = cache.wm.at(read->ml).at(i)[l];
-				assert(isWriteM(res) && sameMl(res, read) &&
-					    res->threadID() == i && res->auxID() != -1);
-				//res->dump();
-				//std::cout<<i<<" "<<std::endl;
-				if(po.hasEdge(write_memory,res)){
-					if(po.hasEdge(res,read)) return {true,false}; // Already reverse edge
-					if(!po.hasEdge(read,res)){
-						po.addEdge(read,res);
-						change = true;
-						added_edges++;
-					}
-				}
-			}else{	// initial obs
-				auto res = cache.wm.at(read->ml).at(i)[0]; // adding edge from first one
-				assert(isWriteM(res) && sameMl(res, read) &&
-					    res->threadID() == i && res->auxID() != -1);
-				if(po.hasEdge(res,read)) return {true,false}; // Already reverse edge
-				if(!po.hasEdge(read,res)){
-					po.addEdge(read,res);
-					change = true;
-					added_edges++;
-				}
-			}
-		}
-	}
-	if(write_memory and writeThread != readThread){
-		int lastBefore = gr.getLatestNotAfterIndex(read,writeThread,po);// {0,1,..,lastBefore} in cache at same ml with r </ wM
-		if(lastBefore != -1){
-			auto cache = gr.getCache();
-			assert(lastBefore < (int) cache.wm.at(read->ml).at(writeThread).size());
-			// Binary search in cache events to get first wM after write
-			int l=0,r=lastBefore;
-			unsigned memId = write_memory->eventID();
-			while(l<r){ // gives r if fail
-				int mid=(l+r)>>1;
-				auto res = cache.wm.at(read->ml).at(writeThread)[mid];
-				assert(isWriteM(res) && sameMl(res, read) &&
-				    res->threadID() == writeThread && res->auxID() != -1);
-				if(res->eventID()>memId) r=mid;
-				else l=mid+1;
-			} // after the loop, l = r = x
-			auto res = cache.wm.at(read->ml).at(writeThread)[l];
-			assert(isWriteM(res) && sameMl(res, read) &&
-				    res->threadID() == writeThread && res->auxID() != -1);
-			if(res->eventID()>memId){
-				if(po.hasEdge(res,read)) return {true,false}; // Already reverse edge
-				if(!po.hasEdge(read,res)){
-					po.addEdge(read,res);
-					change = true;
-				}
-			}
-		}
-	}
-	//std::cout<<"Rule3 "<<change<<"\n";
-	// po.dump();
-	return {false, change}; // done, no change
+std::pair<bool, bool> ZClosure::ruleThree(const ZEvent *read, const ZObs& obs) {
+  // Without optimization
+  auto write = getObs(obs);
+  auto write_memory = write.second;
+  assert(!write_memory || isWriteM(write_memory));
+  // Idea: Iterate on all (but self and writeM) threads, for each thread i:
+  // get 'bad_writeM' - conflicting memory-write successor of write_memory
+  // add edge if not already present, that 'read' -> 'bad_writeM'
+  unsigned totThreads =  ba.number_of_threads();
+  unsigned readThread = read->threadID();
+  unsigned writeThread = write_memory ? write_memory->threadID():INT_MAX;
+  bool change = false;
+  for(unsigned i=0; i < totThreads; ++i) {  // looping over all threads
+    if(i != readThread and i != writeThread) {  // all but read thread
+      int lastBefore = gr.getLatestNotAfterIndex(read,i,po); // {0,1,..,lastBefore} in cache at same ml with r </ wM
+      if(lastBefore == -1) continue;
+      auto &cache = gr.getCache();
+      assert(lastBefore < (int) cache.wm.at(read->ml).at(i).size());
+      if(write_memory) {
+        int l=0,r=lastBefore;
+        // Binary search in cache events to get first wM after write
+        while(l<r) { // gives r if fail
+          int mid=(l+r)>>1;
+          auto res = cache.wm.at(read->ml).at(i)[mid];
+          assert(isWriteM(res) && sameMl(res, read) &&
+                 res->threadID() == i && res->auxID() != -1);
+          if(po.hasEdge(write_memory,res)) r=mid;
+          else l=mid+1;
+        } // after the loop, l = r = x
+        auto res = cache.wm.at(read->ml).at(i)[l];
+        assert(isWriteM(res) && sameMl(res, read) &&
+               res->threadID() == i && res->auxID() != -1);
+        if(po.hasEdge(write_memory,res)) {
+          if(po.hasEdge(res,read))
+            return {true,false}; // Impossible - reverse edge already present
+          if(!po.hasEdge(read,res)) {
+            po.addEdge(read,res);
+            change = true;
+            added_edges++;
+          }
+        }
+      } else {  // initial-event observation
+        auto res = cache.wm.at(read->ml).at(i)[0]; // adding edge from first one
+        assert(isWriteM(res) && sameMl(res, read) &&
+               res->threadID() == i && res->auxID() != -1);
+        if(po.hasEdge(res,read))
+          return {true,false}; // Impossible - reverse edge already present
+        if(!po.hasEdge(read,res)) {
+          po.addEdge(read,res);
+          change = true;
+          added_edges++;
+        }
+      }
+    }
+  }
+  // Proceed as above, on the thread of 'write_memory'
+  if(write_memory and writeThread != readThread) {
+    int lastBefore = gr.getLatestNotAfterIndex(read,writeThread,po); // {0,1,..,lastBefore} in cache at same ml with r </ wM
+    if(lastBefore != -1) {
+      auto cache = gr.getCache();
+      assert(lastBefore < (int) cache.wm.at(read->ml).at(writeThread).size());
+      // Binary search in cache events to get first wM after write
+      int l=0,r=lastBefore;
+      unsigned memId = write_memory->eventID();
+      while(l<r) { // gives r if fail
+        int mid=(l+r)>>1;
+        auto res = cache.wm.at(read->ml).at(writeThread)[mid];
+        assert(isWriteM(res) && sameMl(res, read) &&
+            res->threadID() == writeThread && res->auxID() != -1);
+        if(res->eventID()>memId) r=mid;
+        else l=mid+1;
+      } // after the loop, l = r = x
+      auto res = cache.wm.at(read->ml).at(writeThread)[l];
+      assert(isWriteM(res) && sameMl(res, read) &&
+            res->threadID() == writeThread && res->auxID() != -1);
+      if(res->eventID()>memId) {
+        if(po.hasEdge(res,read))
+          return {true,false}; // Impossible - reverse edge already present
+        if(!po.hasEdge(read,res)) {
+          po.addEdge(read,res);
+          change = true;
+        }
+      }
+    }
+  }
+  // po.dump();
+  return {false, change}; // done, change-variable
 }
 
 
@@ -232,7 +234,7 @@ std::pair<bool, bool> ZClosure::rules(const ZEvent *read, const ZObs& obs){
 /* *************************** */
 /* CLOSE                       */
 /* *************************** */
-bool ZClosure::close(const ZEvent *newread){
+bool ZClosure::close(const ZEvent *newread) {
   // Rules for new read
   if (newread) {
     auto res = rules(newread, an.getObs(newread));
@@ -253,8 +255,7 @@ bool ZClosure::close(const ZEvent *newread){
         return true;
       }
       auto read = ba.getEvent(read_obs.first.thr, -1, read_obs.first.ev);
-      if ((!newread || newread != read) &&
-          !po.isClosureSafe(read)) {
+      if ((!newread || newread != read) && !po.isClosureSafe(read)) {
         auto res = rules(read, read_obs.second);
         if (res.first) { return false; }
         if (res.second) {
@@ -266,9 +267,9 @@ bool ZClosure::close(const ZEvent *newread){
     cur++;
     assert(cur == (int) an.size());
     if (cur == last_change) {
-        // One entire iteration without any changes
-        // hence the partial order is closed
-        return true;
+      // One entire iteration without any changes
+      // hence the partial order is closed
+      return true;
     }
     if (newread) {
       auto res = rules(newread, an.getObs(newread));
@@ -290,7 +291,7 @@ bool ZClosure::close(const ZEvent *newread){
 /* PRE-CLOSE                   */
 /* *************************** */
 
-void ZClosure::preClose(const ZEvent *ev, const ZEvent *obsEv){
+void ZClosure::preClose(const ZEvent *ev, const ZEvent *obsEv) {
   assert(sameMl(ev, obsEv));
   assert((isRead(ev) && isWriteB(obsEv)) ||
          (isLock(ev) && isUnlock(obsEv)));
@@ -306,28 +307,30 @@ void ZClosure::preClose(const ZEvent *ev, const ZEvent *obsEv){
     auto obsMem = obsEv->write_other_ptr;
     assert(isWriteM(obsMem));
     assert(!po.hasEdge(ev, obsMem));
-    if (!po.hasEdge(obsMem, ev)) po.addEdge(obsMem, ev);
-	// Adding edge from Memory write of largest (by eventid) Buffer write which happended before current read i.e. ev
+    if (!po.hasEdge(obsMem, ev))
+      po.addEdge(obsMem, ev);
+    // Edge to 'obsMem' from memory-write of largest (by eventid)
+    // local buffer write which happens before read 'ev'
     auto lastBuf = gr.getLocalBufferW(ev);
-    if(lastBuf){
-    	assert(isWriteB(lastBuf));
-    	auto Mem_counterpart = lastBuf->write_other_ptr;	// Getting the Memory Write
-    	assert(isWriteM(Mem_counterpart));
-    	assert(!po.hasEdge(obsMem,Mem_counterpart));
-		if(!po.hasEdge(Mem_counterpart,obsMem)){
-			po.addEdge(Mem_counterpart,obsMem);		// Memory write before curr obs
-		}
+    if(lastBuf) {
+      assert(isWriteB(lastBuf));
+      auto mem_counterpart = lastBuf->write_other_ptr; // Getting the Memory Write
+      assert(isWriteM(mem_counterpart));
+      assert(!po.hasEdge(obsMem, mem_counterpart));
+      if(!po.hasEdge(mem_counterpart, obsMem)) {
+        po.addEdge(mem_counterpart, obsMem); // Memory write before curr obs
+      }
     }
   }
   // po.dump();
 }
 
-void ZClosure::preClose(const ZEvent *ev, const ZObs& obs){
+void ZClosure::preClose(const ZEvent *ev, const ZObs& obs) {
   if (obs.thr == INT_MAX) {
-  	// Handle initial-event observation separately
-  	// No need to do for rule 1
-  }else{
-  	preClose(ev, po.basis.getEvent(obs));
+    // Handle initial-event observation separately
+    // Nothing to do in preClosure (rule1)
+  } else {
+    preClose(ev, po.basis.getEvent(obs));
   }
   // po.dump();
 }
